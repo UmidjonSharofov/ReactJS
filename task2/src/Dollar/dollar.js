9560230df4a94ee134e591184ff0d56661374b4c
umid <umidshrofov@gmail.com>
2023-02-23 19:31:38 +0500

    1vazifa tugadi

--- task2/src/Dollar/dollar.js
+++ task2/src/Dollar/dollar.js
@@ -10,15 +10,19 @@ class Dollar extends React.Component {
   }
   render(){ 
     let qm=0
+    let krqm=0
     let qimat=(a)=>{
      qm=+a.target.value * 15000
+     krqm=a.target.value
     }
     let clik =()=>{
-     this.setState({count: qm})
+     this.setState({count: `Siz kiritgan ${krqm}$ O'zbek sumida ${qm} so'mini tashkil qiladi!`})
     }
     return(
         <div className="dollor">
+          <div className="box">
            <><h2>Dollarni Kiriting </h2><input onChange={qimat} type="number"></input> <h2>1$ O'zbek summasida  15000 summaini tasgkil etadi</h2><button onClick={clik} >Transfer</button></>
+          </div>
            <h2 className="h2">{this.state.count}</h2>
        </div>
     )

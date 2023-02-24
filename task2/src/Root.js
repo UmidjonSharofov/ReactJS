f176871bcb28fc00a4b87696be8992bcc8c77224
umid <umidshrofov@gmail.com>
2023-02-24 20:34:19 +0500

    tugadi

--- task2/src/Root.js
+++ task2/src/Root.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Dollar/dollar";
-import Dollar from "./Dollar/dollar";
 import './young/index'
+import Dollar from "./Dollar/dollar";
 import Yosh from "./young/index";
+
+
 
 function Root() {
   return (

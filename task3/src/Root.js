7333d428d4f580ac90e70ec26b9ebd3b7493b88c
umid <umidshrofov@gmail.com>
2023-02-25 10:53:54 +0500

    task3 boshlandi

---
+++ task3/src/Root.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./task1/index"
+import Square from "./task1/index";
+
+
+export default function Root(){
+    return(
+        <Square/>
+    )
+}
---
+++ task3/src/task1/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-useless-constructor */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+export default  class Square extends React.Component {
+    constructor (props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <h2>nma</h2>
+        )
+    }
+}

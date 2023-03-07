f74232ff7181a017d47577a70841e6bb290759f2
umid <umidshrofov@gmail.com>
2023-03-07 11:32:00 +0500

    nav tugadi

---
+++ task/src/root.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Fetch from './json'
+
+class Root extends Component {
+    render () {
+        return (
+           <Fetch/>
+        )
+    }
+}
+
+export default Root
